230f1163b29ab6fe0b51e17335b7e3df57da178a
Fazil <faziltirtana@tutanota.com>
2025-02-12 20:59:06 +0700

    feat: add edit product in ProductController

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -35,4 +35,21 @@ public class ProductController {
         model.addAttribute("products", allProducts);
         return "productList";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable("id") String id, Model model) {
+        Product product = service.findById(id);
+        if (product == null) {
+            return "redirect:/product/list";
+        }
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProductPost(@ModelAttribute Product product, @PathVariable("id") String id) {
+        product.setProductId(id);
+        service.update(product);
+        return "redirect:/product/list";
+    }
 }
