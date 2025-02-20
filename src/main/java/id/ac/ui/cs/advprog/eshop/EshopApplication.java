8a1dd844de0e3c1bb12ad2e9dfd106f86fb8b32c
Fazil <faziltirtana@tutanota.com>
2025-02-20 17:38:09 +0700

    fix: remove explicit constructor and suppress utility class warning in EshopApplication
    - Removed the unnecessary public no-arg constructor.
    - The default public constructor is generated automatically, satisfying Spring Boot.
    - Added @SuppressWarnings("PMD.UseUtilityClass") to suppress the PMD warning.

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -3,10 +3,9 @@ package id.ac.ui.cs.advprog.eshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("PMD.UseUtilityClass")
 @SpringBootApplication
 public class EshopApplication {
-
-    public EshopApplication() {}
 
     public static void main(String[] args) {
         SpringApplication.run(EshopApplication.class, args);
